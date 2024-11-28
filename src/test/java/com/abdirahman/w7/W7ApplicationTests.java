526ae09745ce76e7ee086a5eb84d47b9b8dd2e1c
Abdirahman-oz <aosman31@myseneca.ca>
2024-11-28 01:13:20 -0500

    skeleton

---
+++ src/test/java/com/abdirahman/w7/W7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdirahman.w7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
