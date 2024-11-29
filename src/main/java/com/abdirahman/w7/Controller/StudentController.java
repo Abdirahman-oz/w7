85742ec462cd288c6a724d22de7f41b08132671d
Abdirahman-oz <aosman31@myseneca.ca>
2024-11-28 21:47:44 -0500

    updated Controller

--- src/main/java/com/abdirahman/w7/Controller/StudentController.java
+++ src/main/java/com/abdirahman/w7/Controller/StudentController.java
@@ -7,7 +7,44 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-
-
+@RestController
+@RequestMapping("/api/students")
 public class StudentController {
+    private final StudentService studentService;
+
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getAllStudent(){
+        return  studentService.getALLStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getOneStudent(@PathVariable long id){
+        return  studentService.getOneStudent(id);
+    }
+
+    @PutMapping ("/{id}")
+    public Student updateStudent(@PathVariable long id,Student student){
+        return studentService.updateStudent(id,student);
+    }
+
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student){
+        return  studentService.savePerson(student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteOneStudent(@PathVariable long id){
+         studentService.deleteStudent(id);
+
+    }
+
+
+
+
 }
--- src/main/java/com/abdirahman/w7/W7Application.java
+++ src/main/java/com/abdirahman/w7/W7Application.java
@@ -3,6 +3,7 @@ package com.abdirahman.w7;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class W7Application {
 
